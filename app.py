59de997efcb450a7ac29916c39c9b58b19b97564
Альбинас Гапшис <student@FB.LOCAL>
2024-09-07 13:14:51 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,4 +3,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask"
+    return """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Гапшис Аьбинас Альбинович, Лабораторная 1</title>
+</head>
+<body>
+    <header>
+        НГТУ, ФБ, лабораторная работа 1
+    </header>
+
+    <h1>web-сервер на flask</h1>
+
+    <footer>
+        &copy; Альбинас Гапшис, ФБИ-23, 3 курс, 2024
+    </footer>
+</body>
+</html>
+"""
