5236a9bafb12f20b807c98961a680c0796e3bb15
Альбинас Гапшис <student@FB.LOCAL>
2024-09-07 13:30:05 +0700

    Меню

--- app.py
+++ app.py
@@ -2,7 +2,30 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/index")
 def start():
+    return """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>НГТУ, ФБ, Лабораторные работы</title>
+</head>
+<body>
+    <header>
+        НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
+    </header>
+
+    <h1>web-сервер на flask</h1>
+    <li> <a href="/lab1"> Лабораторная работа 1 </a></li>
+    <footer>
+        &copy; Альбинас Гапшис, ФБИ-23, 3 курс, 2024
+    </footer>
+</body>
+</html>
+"""
+
+@app.route("/lab1")
+def lab1():
     return """
 <!DOCTYPE html>
 <html>
@@ -15,7 +38,13 @@ def start():
     </header>
 
     <h1>web-сервер на flask</h1>
-
+    <div>
+        Flask — фреймворк для создания веб-приложений на языке
+        программирования Python, использующий набор инструментов
+        Werkzeug, а также шаблонизатор Jinja2. Относится к категории так
+        называемых микрофреймворков — минималистичных каркасов веб-приложений, сознательно предоставляющих лишь самые ба-
+        зовые возможности.
+    </div>
     <footer>
         &copy; Альбинас Гапшис, ФБИ-23, 3 курс, 2024
     </footer>
